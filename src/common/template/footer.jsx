083aa9fe7e3fbbbf661890f131e46456d6c0f17a
Vinicius Rodrigues <vini0593@gmail.com>
2023-04-14 14:43:01 -0300

    component footer

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => {
+  return (
+    <footer className="main-footer">
+      <strong>
+        copytight &copy; 2023
+        <a href="https://github.com/henrvini" target="_blank">
+          henrvini
+        </a>
+      </strong>
+    </footer>
+  );
+};
