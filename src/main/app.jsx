dafbce4220a1240a4d9bd5272ba29af210fcf0b5
Vinicius Rodrigues <vini0593@gmail.com>
2023-04-14 15:19:45 -0300

    billinCycle, dashboard, routes, first config

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,16 +1,17 @@
 import "../common/template/dependencies";
 import React from "react";
 
+import Routes from "./routes";
 import Header from "../common/template/header";
-import SideBar from "../common/template/sideBar";
 import Footer from "../common/template/footer";
+import SideBar from "../common/template/sideBar";
 
 export default (props) => (
   <div className="wrapper">
     <Header />
     <SideBar />
     <div className="content-wrapper">
-      <h1>Conteudo</h1>
+      <Routes />
     </div>
     <Footer />
   </div>
