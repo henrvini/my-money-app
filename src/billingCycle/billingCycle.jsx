4b1c0dcf603dc9eb2d8d4e8f7b35bd6366fe11ca
Vinicius Rodrigues <vini0593@gmail.com>
2023-04-17 15:12:23 -0300

    tabHeader component

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 
-import Content from "../common/template/content";
-import ContentHeader from "../common/template/contentHeader";
 import Tabs from "../common/tab/tabs";
+import Content from "../common/template/content";
 import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
+import ContentHeader from "../common/template/contentHeader";
+import TabHeader from "../common/tab/tabHeader";
 
 class BillingCycle extends Component {
   render() {
@@ -13,7 +14,12 @@ class BillingCycle extends Component {
         <ContentHeader title="Ciclos de Pagamento" small="Cadastro" />
         <Content>
           <Tabs>
-            <TabsHeader></TabsHeader>
+            <TabsHeader>
+              <TabHeader label="Listar" icon="bars" target="tabList" />
+              <TabHeader label="Incluir" icon="plus" target="tabCreate" />
+              <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
+              <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
+            </TabsHeader>
             <TabsContent></TabsContent>
           </Tabs>
         </Content>
