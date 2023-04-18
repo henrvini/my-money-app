d4c06d3b77901f13ac660082f45fcd7fe64e4950
Vinicius Rodrigues <vini0593@gmail.com>
2023-04-18 10:46:28 -0300

    billingCycle actions and reducer

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import TabReducer from "../common/tab/tabReducer";
 import DashboardReducer from "../dashboard/dashboardReducer";
+import BillingCycleReducer from "../billingCycle/billingCycleReducer";
 
 const rootReducer = combineReducers({
   dashboard: DashboardReducer,
   tab: TabReducer,
+  list: BillingCycleReducer,
 });
 
 export default rootReducer;
