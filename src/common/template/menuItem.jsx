49ba35b555a12a303ebec0a3531d4b5c8c36e73f
Vinicius Rodrigues <vini0593@gmail.com>
2023-04-20 13:56:09 -0300

    fixed responsivesses

--- src/common/template/menuItem.jsx
+++ src/common/template/menuItem.jsx
@@ -5,7 +5,7 @@ export default (props) => {
     <li>
       <a href={props.path}>
         <i className={`fa fa-${props.icon}`}></i>
-        {props.label}
+        <span>{props.label}</span>
       </a>
     </li>
   );
